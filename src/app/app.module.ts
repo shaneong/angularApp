1e936489339d1c2b0d49c84d80283f7b513832f6
Shane Ong <ongxx103@umn.edu>
2017-05-28 13:05:32 +0800

    Tour of Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+
+// Imports for loading & configuring the in-memory web api
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+  this.router.navigate(['/detail', this.selectedHero.id]);
+}
+}
+
